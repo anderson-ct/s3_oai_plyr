425dcb1969b7abd19c726cf4e9e8b9923fe845e2
Ubuntu <ubuntu@ip-172-31-73-130.ec2.internal>
2021-01-01 13:08:24 +0000

    aws cognito hosted UI with sign in & sign up urls

---
+++ Media_Convert_PoC/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Media_Convert_PoC project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Media_Convert_PoC.settings')
+
+application = get_wsgi_application()
